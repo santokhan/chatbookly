a6d1987f268e754822d4f789be232b8489cb698b
santokhan <santokhan1999@gmail.com>
2025-08-14 00:15:48 +0600

    Make Responsive

--- src/components/layouts/parts/HeaderProfile.vue
+++ src/components/layouts/parts/HeaderProfile.vue
@@ -5,11 +5,13 @@
         <img src="/assets/images/placeholder-profile.png" alt="profile" class="w-full h-full object-cover" />
       </figure>
       <p class="hidden md:block">Jhon Marco</p>
-      <svg class="size-6" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M12.0001 13.0546L16.4551 8.59961L17.7277 9.87221L12.0001 15.5998L6.27246 9.87221L7.54506 8.59961L12.0001 13.0546Z"
-          fill="#99A0AE" />
-      </svg>
+      <div class="hidden md:block">
+        <svg class="size-6" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M12.0001 13.0546L16.4551 8.59961L17.7277 9.87221L12.0001 15.5998L6.27246 9.87221L7.54506 8.59961L12.0001 13.0546Z"
+            fill="#99A0AE" />
+        </svg>
+      </div>
     </button>
   </div>
 </template>
--- src/components/layouts/parts/TheHeader.vue
+++ src/components/layouts/parts/TheHeader.vue
@@ -6,7 +6,7 @@ import HeaderProfile from './HeaderProfile.vue';
 </script>
 
 <template>
-  <div class="flex items-center h-[var(--height-header)] px-4 lg:px-8 border-b border-soft-200 gap-3">
+  <div class="flex items-center h-[var(--height-header)] px-4 lg:px-8 border-b border-soft-200 gap-2 lg:gap-3">
     <HeaderLogo />
     <div class="grow"></div>
     <HeaderPlan />
