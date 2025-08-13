008363098e85ea7ce01802e64eb4f074ddf9cb25
santokhan <santokhan1999@gmail.com>
2025-08-14 00:11:50 +0600

    Make responsive

--- src/components/layouts/parts/HeaderProfile.vue
+++ src/components/layouts/parts/HeaderProfile.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="relative">
     <button type="button" class="text-sm font-medium flex items-center gap-2">
-      <figure class="size-9">
-        <img src="/assets/images/placeholder-profile.png" class="w-full h-full object-cover" />
+      <figure class="size-9" title="Jhon Marco">
+        <img src="/assets/images/placeholder-profile.png" alt="profile" class="w-full h-full object-cover" />
       </figure>
-      <p>Jhon Marco</p>
+      <p class="hidden md:block">Jhon Marco</p>
       <svg class="size-6" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M12.0001 13.0546L16.4551 8.59961L17.7277 9.87221L12.0001 15.5998L6.27246 9.87221L7.54506 8.59961L12.0001 13.0546Z"
